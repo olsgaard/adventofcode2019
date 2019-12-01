9cfbcb37127be3318b51100c819ff1e71ca5d728
Mads Ølsgaard <supermads@Madss-MacBook-Pro.local>
2019-12-01 21:41:14 +0100

    Add function and asserts, working. Solving for input.txt not tested yet

---
+++ solve_puzzle01.py
@@ -0,0 +1,21 @@
+"""
+Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+For example:
+For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+For a mass of 1969, the fuel required is 654.
+For a mass of 100756, the fuel required is 33583.
+"""
+
+def calculate_fuel(mass):
+  return int(mass / 3) - 2
+
+assert calculate_fuel(12) == 2
+assert calculate_fuel(14) == 2
+assert calculate_fuel(1969) == 654
+assert calculate_fuel(100756) == 33583 
+
+with open("input.txt") as f:
+    result = sum([calculate_fuel(mass) for mass in f.readlines()])
+
+print(result)
