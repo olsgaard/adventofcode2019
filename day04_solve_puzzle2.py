980cb4bae43e37afc706ac3cd2c18f21846e819c
Mads Ølsgaard <mads.olsgaard@computas.com>
2019-12-05 16:55:46 +0100

    Add solution to puzzle 2, day 4. Fresh outlook and more detailed testing finally gave a correct solution.

--- day04_solve_puzzle2.py
+++ day04_solve_puzzle2.py
@@ -1,9 +1,39 @@
+def is_ascending(number):
+	ns = str(number)
+	return all([ns[i] >= ns[i-1] for i in range(1, len(ns))])
+
+assert is_ascending(123456) == True
+assert is_ascending(1234444456) == True
+assert is_ascending(1234566666) == True
+assert is_ascending(1111234566666) == True
+
+assert is_ascending(1234566566) == False
+assert is_ascending(112211) == False
+
+def has_lonely_pair(number):
+	ns = "x" + str(number) + "x"
+	lonely_pairs = []
+	for i in range(1,len(ns)-2):
+		lonely_pairs.append((ns[i-1] != ns[i]) and (ns[i] == ns[i+1]) and (ns[i+1] != ns[i+2]))
+	return any(lonely_pairs)
+
+assert has_lonely_pair(112233) == True
+assert has_lonely_pair(132213) == True
+assert has_lonely_pair(111233) == True
+assert has_lonely_pair(123433) == True
+
+assert has_lonely_pair(111234) == False
+assert has_lonely_pair(111222) == False
+assert has_lonely_pair(123444) == False
 
 def check_exanded_rules(password):
-	pass
+	return is_ascending(password) and has_lonely_pair(password)
 
 def count_valid_passwords(_min, _max):
-	pass
+	""" The min/max values ensures that the length and the range is within the 
+	password definition, and there is no need to check for it explicitly """
+	
+	return sum([check_exanded_rules(password) for password in range(_min, _max+1)])
 
 
 # Asserts from problem description
@@ -11,12 +41,13 @@ assert check_exanded_rules(112233) == True
 assert check_exanded_rules(123444) == False
 assert check_exanded_rules(111122) == True
 
+assert check_exanded_rules(223450) == False
+assert check_exanded_rules(123789) == False
+
 # These are asserts are "hand calculated"
 assert check_exanded_rules(111123) == False
 assert check_exanded_rules(122333) == True
 assert check_exanded_rules(111111) == False
-assert check_exanded_rules(223450) == False
-assert check_exanded_rules(123789) == False
 assert check_exanded_rules(123788) == True
 assert check_exanded_rules(889999) == True
 assert check_exanded_rules(112345) == True
