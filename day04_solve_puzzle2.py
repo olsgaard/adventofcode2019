82dc41cbed3a56b2ac3061e33c0d297a610bbf67
Mads Ølsgaard <mads.olsgaard@computas.com>
2019-12-05 16:34:23 +0100

    Add skeleton

--- day04_solve_puzzle2.py
+++ day04_solve_puzzle2.py
@@ -1,7 +1,17 @@
+
+def check_exanded_rules(password):
+	pass
+
+def count_valid_passwords(_min, _max):
+	pass
+
+
+# Asserts from problem description
 assert check_exanded_rules(112233) == True
 assert check_exanded_rules(123444) == False
 assert check_exanded_rules(111122) == True
 
+# These are asserts are "hand calculated"
 assert check_exanded_rules(111123) == False
 assert check_exanded_rules(122333) == True
 assert check_exanded_rules(111111) == False
@@ -14,3 +24,5 @@ assert check_exanded_rules(122345) == True
 assert check_exanded_rules(112311) == False
 assert check_exanded_rules(111111) == False
 assert check_exanded_rules(123456) == False
+
+print(count_valid_passwords(146810, 612564))
